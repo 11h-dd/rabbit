8db936c72c89eaa91ce9be426d8712a573c7538d
xuxu <1438102025@qq.com>
2022-09-22 15:59:45 +0800

    初始化

---
+++ src/components/XtxLayout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import XtxTopNav from "./XtxTopNav.vue";
+import XtxHeader from "./XtxHeader.vue";
+import XtxFooter from "./XtxFooter.vue";
+
+</script>
+    
+<template>
+
+    <XtxTopNav />
+    <XtxHeader />
+    <RouterView />
+    <XtxFooter />
+</template>
+    
+<style scoped lang="less">
+
+</style>
